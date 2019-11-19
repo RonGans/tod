60b6da564bf842c4f8dac65ae60dcce16dd61e39
RonGans <ronganssb@gmail.com>
2019-11-18 22:43:39 -0800

    Comment out more code

--- lib/tod/time_of_day_type.rb
+++ lib/tod/time_of_day_type.rb
@@ -1,11 +1,11 @@
-module Tod
-  class TimeOfDayType < ActiveModel::Type::Value
-    def cast(value)
-      TimeOfDay.load(value)
-    end
+#module Tod
+#  class TimeOfDayType < ActiveModel::Type::Value
+#    def cast(value)
+#      TimeOfDay.load(value)
+#    end
 
-    def serialize(value)
-      TimeOfDay.dump(value)
-    end
-  end
-end
+#    def serialize(value)
+#      TimeOfDay.dump(value)
+#    end
+#  end
+#end
